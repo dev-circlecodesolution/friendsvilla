6bd5eceabdcb0dbac860b93988a050ac1370d7e3
aafan-kadri-123 <aafan.kadri123@gmail.com>
2020-11-27 23:25:36 +0530

    Changes are done in Contact Us, footer, Know us better, Safety come first, Things to Know, 3, 5, 30, 29

---
+++ main.js
@@ -0,0 +1,58 @@
+	$(".connect-link").hover(
+		function(){ 
+			$(".arrow-connect").show();
+		},
+		function(){
+			$(".arrow-connect").hide();			
+		}
+	);
+
+	$(".explore-link").hover(
+		function(){ 
+			$(".arrow-explore").show();
+		},
+		function(){
+			$(".arrow-explore").hide();			
+		}
+	);
+
+	$(".read-link").hover(
+		function(){ 
+			$(".arrow-read").show();
+		},
+		function(){
+			$(".arrow-read").hide();			
+		}
+	);
+
+
+	$(".safety-link").hover(
+		function(){ 
+			$(".arrow-safety").show();
+		},
+		function(){
+			$(".arrow-safety").hide();			
+		}
+	);	
+
+	$(".host-link").hover(
+		function(){ 
+			$(".arrow-host").show();
+		},
+		function(){
+			$(".arrow-host").hide();			
+		}
+	);	
+
+
+	$(".join-link").hover(
+		function(){ 
+			$(".arrow-join").show();
+		},
+		function(){
+			$(".arrow-join").hide();			
+		}
+	);	
+
+
+
